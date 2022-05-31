df4c23bbf9a8836084fee1599a8e1102dc1122e0
David Sánchez <davidsanchez@Davids-MacBook-Pro.local>
2022-05-31 14:51:57 -0500

    set state for input field

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,24 +1,64 @@
 
-import { SaveAltOutlined } from '@mui/icons-material'
-import { Button } from '@mui/material'
+import { Add, SaveAltOutlined } from '@mui/icons-material'
+import { Button, TextField } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 export const NewEntry = () => {
-  return (
-    <Box display='flex' justifyContent='space-between'>
-        <Button
-            variant='text'
-        >
-            Cancelar
-        </Button>
-        <Button
-            variant='outlined'
-            color='secondary'
-            endIcon={<SaveAltOutlined/>}
-        >
-            Guardar
-        </Button>
-    </Box>
-  )
+
+    const [isAdding, setIsAdding] = useState(false);
+    const [touched, setTouched] = useState(false);
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputValue= (e:ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+    return (
+        <Box sx={{marginBottom: 2, paddingX: 2}}>
+            {
+                isAdding ? (
+                    <>
+                        <TextField 
+                            fullWidth
+                            sx={{marginTop:2, marginBottom:1}}
+                            placeholder='Nueva Entrada'
+                            autoFocus
+                            multiline
+                            label='Nueva Entrada'
+                            helperText={ inputValue.length <= 0 && touched && 'Ingrese un valor'}
+                            error={ inputValue.length <= 0 && touched}
+                            onChange={handleInputValue}
+                            onBlur={() => setTouched(true)}
+                        />
+                        <Box display='flex' justifyContent='space-between'>
+                            <Button
+                                variant='text'
+                                onClick={() => setIsAdding(false)}
+                            >
+                                Cancelar
+                            </Button>
+                            <Button
+                                variant='outlined'
+                                color='secondary'
+                                endIcon={<SaveAltOutlined/>}
+                            >
+                                Guardar
+                            </Button>
+                        </Box>
+                    </>
+                ) : (
+                    <Button
+                        startIcon={<Add />}
+                        fullWidth
+                        variant='outlined'
+                        onClick={() => setIsAdding(true)}
+                    >
+                        Agregar Tarea
+                    </Button>
+                )
+            }
+
+
+        </Box>
+    )
 }
